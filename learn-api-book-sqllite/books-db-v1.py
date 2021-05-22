f62a1120443985e0a60a82fc3c1f1dd4a6f6f37a
Satish Atakare <satakare1@gmail.com>
2021-05-22 23:34:05 +0530

    Adding support for Like clause with new api

--- learn-api-book-sqllite/books-db-v1.py
+++ learn-api-book-sqllite/books-db-v1.py
@@ -17,6 +17,9 @@ def home():
     return '''<h1>Distant Reading Archive</h1>
 <p>A prototype API for distant reading of science fiction novels.</p>'''
 
+@app.route('/about', methods=['GET'])
+def about():
+    return '''<h1>Satish Atakare</h1>'''
 
 @app.route('/api/v2/resources/books/all', methods=['GET'])
 def api_all():
@@ -34,13 +37,15 @@ def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 
-@app.route('/api/v2/resources/books', methods=['GET'])
+@app.route('/api/v1/resources/books', methods=['GET'])
 def api_filter():
     query_parameters = request.args
+    print(request.args)
 
     id = query_parameters.get('id')
     published = query_parameters.get('published')
     author = query_parameters.get('author')
+    title = query_parameters.get('title')
 
     query = "SELECT * FROM books WHERE"
     to_filter = []
@@ -54,7 +59,10 @@ def api_filter():
     if author:
         query += ' author=? AND'
         to_filter.append(author)
-    if not (id or published or author):
+    if title:
+        query += ' title=? AND'
+        to_filter.append(title)
+    if not (id or published or author or title):
         return page_not_found(404)
 
     query = query[:-4] + ';'
@@ -67,4 +75,49 @@ def api_filter():
 
     return jsonify(results)
 
+#---------------------------------------------------
+@app.route('/api/v1/resources/books_like', methods=['GET'])
+def api_filter_like():
+    query_parameters = request.args
+    print(request.args)
+
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
+    title = query_parameters.get('title')
+
+    query = "SELECT * FROM books WHERE"
+    to_filter = []
+
+#cur.execute("select * from contacts where name like ?", ('%'+search+'%',))
+#LIKE using from python is crazy
+
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += " published like ? AND"
+        to_filter.append(published)
+    if author:
+        query += " author like ? AND"
+        to_filter.append(author)
+    if title:
+        query += ' title LIKE ? AND'
+        to_filter.append('%'+title+'%')
+    if not (id or published or author or title):
+        return page_not_found(404)
+
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    #print(to_filter)
+    results = cur.execute(query, to_filter).fetchall()
+
+    return jsonify(results)
+
+
+###---------------------------------------------------------------------
 app.run()
